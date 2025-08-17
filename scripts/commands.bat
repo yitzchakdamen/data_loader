1073ead87795be7fb811c122ab78799dec9cd423
yitzchakdamen <yitzchakdamen@gmail.com>
2025-08-17 11:43:22 +0300

    Update data-loader image tag to v2 in deployment and build scripts

--- scripts/commands.bat
+++ scripts/commands.bat
@@ -7,9 +7,8 @@ oc apply -f mysql-service.yaml
 oc apply -f sql-pvs.yaml
 
 docker build -t data_loader .
-docker tag data_loader yitzchakdamen/data_loadr:v1
-docker push yitzchakdamen/data_loadr:v1
-
+docker tag data_loader yitzchakdamen/data_loadr:v2
+docker push yitzchakdamen/data_loadr:v2
 
 oc apply -f data_loader-deployment.yaml
 oc apply -f data_loader-service.yaml
